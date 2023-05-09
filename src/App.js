f8b2502e2e433f730908d32e4e372d16efdf0a65
Shodipo Ayomide <shodipovi@gmail.com>
2023-05-10 00:32:08 +0100

    Chakra UI ROCKSSSSSSSSS, Wo Hooooo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from 'react';
 import { ethers } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
-import { Box, Button, VStack, HStack, Text, Textarea, useToast, Spinner } from "@chakra-ui/react";
+import { Box, Button, VStack, HStack, Text, Textarea, useToast, Spinner, Flex, Heading } from "@chakra-ui/react";
 
 const contractABI = [
   { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "uint256", "name": "messageId", "type": "uint256" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": false, "internalType": "string", "name": "content", "type": "string" }, { "indexed": false, "internalType": "uint256", "name": "timestamp", "type": "uint256" }], "name": "NewMessage", "type": "event" },
@@ -121,40 +121,52 @@ function App() {
   }, [provider, fetchMessages, toast]);
 
   return (
-    <VStack padding={4}>
-      <Button colorScheme="teal" onClick={handleLogin}>Login with MetaMask</Button>
-      <VStack spacing={4} align="stretch">
-        {messages.length > 0 ? (
-          messages.map((message, index) => (
-            <Box key={index} border="1px" borderRadius="md" padding={4}>
-              <HStack spacing={4}>
-                <Text>Sender:</Text>
-                <Text>{message.sender}</Text>
-              </HStack>
-              <HStack spacing={4}>
-                <Text>Content:</Text>
-                <Text>{message.content}</Text>
-              </HStack>
-              <HStack spacing={4}>
-                <Text>Timestamp:</Text>
-                <Text>{new Date(message.timestamp * 1000).toLocaleString()}</Text>
-              </HStack>
-            </Box>
-          ))
-        ) : (
-          <Text>No messages yet...</Text>
-        )}
-      </VStack>
-      <HStack spacing={4}>
-        <Textarea
-          value={newMessage}
-          onChange={e => setNewMessage(e.target.value)}
-          placeholder="Enter your message here"
-        />
-        <Button colorScheme="teal" onClick={handleSendMessage} isLoading={loading}>Send Message</Button>
-      </HStack>
-      {loading && <Spinner />}
-    </VStack>
+    <Flex 
+      direction="column" 
+      align="center" 
+      justify="center" 
+      minHeight="100vh" 
+      bg="teal.500"
+      color="white"
+    >
+      <Box maxWidth="800px" width="100%" padding={4} bg="whiteAlpha.200" borderRadius="md">
+        <Heading mb={6} textAlign="center">Web3 Messaging App</Heading>
+        <VStack spacing={4} align="stretch">
+          <Button colorScheme="orange" onClick={handleLogin}>Login with MetaMask</Button>
+          {messages.length > 0 ? (
+            messages.map((message, index) => (
+              <Box key={index} border="1px" borderRadius="md" padding={4} bg="whiteAlpha.800" color="black">
+                <HStack spacing={4}>
+                  <Text fontWeight="bold">Sender:</Text>
+                  <Text>{message.sender}</Text>
+                </HStack>
+                <HStack spacing={4}>
+                  <Text fontWeight="bold">Content:</Text>
+                  <Text>{message.content}</Text>
+                </HStack>
+                <HStack spacing={4}>
+                  <Text fontWeight="bold">Timestamp:</Text>
+                  <Text>{new Date(message.timestamp * 1000).toLocaleString()}</Text>
+                </HStack>
+              </Box>
+            ))
+          ) : (
+            <Text>No messages yet...</Text>
+          )}
+          <VStack spacing={4} align="stretch">
+            <Textarea
+              value={newMessage}
+              onChange={e => setNewMessage(e.target.value)}
+              placeholder="Enter your message here"
+              bg="whiteAlpha.800"
+              color="black"
+            />
+            <Button colorScheme="green" onClick={handleSendMessage} isLoading={loading}>Send Message</Button>
+          </VStack>
+        </VStack>
+        {loading && <Spinner />}
+      </Box>
+    </Flex>
   );
 }
 
